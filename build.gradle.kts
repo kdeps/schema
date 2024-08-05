a61fbf5c4b549382ebff81cb6d66eeeaf4db715e
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2024-08-06 00:07:33 +0200

    Added initial core schema

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+  id("org.pkl-lang") version "0.25.2"
+}
+
+val maybeVersion = System.getenv("VERSION")
+
+pkl {
+  project {
+    packagers {
+      register("makePackages") {
+        if (maybeVersion != null) {
+          environmentVariables.put("VERSION", maybeVersion)
+        }
+        projectDirectories.from(file("schema/"))
+      }
+    }
+  }
+  // ./gradlew pkldoc
+  if (maybeVersion != null) {
+    pkldocGenerators {
+      register("pkldoc") {
+        sourceModules =
+          listOf(uri("package://schema.kdeps.com/schema/$maybeVersion"))
+      }
+    }
+  }
+}
