bda10a4bc2c756910bb52dd8527c157c9db5b668
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2024-11-14 08:55:42 +0100

    added install Anaconda option

--- gen/docker/DockerSettings.pkl.go
+++ gen/docker/DockerSettings.pkl.go
@@ -4,6 +4,12 @@ package docker
 // Class representing the settings for Docker configurations.
 // It includes options for specifying packages, PPAs, and models.
 type DockerSettings struct {
+	// Sets if Anaconda3 will be pre-installed in the Image
+	InstallAnaconda bool `pkl:"installAnaconda"`
+
+	// Python packages that will be pre-installed using Anaconda3.
+	PythonPackages *[]string `pkl:"pythonPackages"`
+
 	// A list of packages to be installed in the Docker container.
 	Packages *[]string `pkl:"packages"`
 
