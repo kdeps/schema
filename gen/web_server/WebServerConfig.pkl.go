3b35f51afba1db7a46b387addce46beb1bd18778
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-04-18 18:33:28 +0200

    add trustedproxy on webserver

--- gen/web_server/WebServerConfig.pkl.go
+++ gen/web_server/WebServerConfig.pkl.go
@@ -9,6 +9,13 @@ type WebServerConfig struct {
 	// The port the server listens on (default: 8080)
 	Port uint16 `pkl:"port"`
 
+	// A list of trusted proxies (IPv4, IPv6, or CIDR ranges).
+	// If set, only requests passing through these proxies will have their `X-Forwarded-For`
+	// header trusted.
+	// If unset, all proxies—including potentially malicious ones—are considered trusted,
+	// which may expose the server to IP spoofing and other attacks.
+	TrustedProxies *[]string `pkl:"trustedProxies"`
+
 	// List of routes configured for the server
 	//
 	// Each route specifies a path and its server behavior
