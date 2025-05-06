4f41f050928c3025f33e7cd91dc030daa3b20aee
Joel Bryan Juliano <joelbryan.juliano@gmail.com>
2025-05-06 20:38:15 +0200

    add the initial memory record item read and update function

---
+++ gen/memory/init.pkl.go
@@ -0,0 +1,9 @@
+// Code generated from Pkl module `org.kdeps.pkl.Memory`. DO NOT EDIT.
+package memory
+
+import "github.com/apple/pkl-go/pkl"
+
+func init() {
+	pkl.RegisterMapping("org.kdeps.pkl.Memory", MemoryImpl{})
+	pkl.RegisterMapping("org.kdeps.pkl.Memory#MemoryRecord", MemoryRecord{})
+}
